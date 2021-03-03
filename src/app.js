ec85845d5aee4f3e2116c4c1ed89baeb2aeba1f5
Kallum Tolkien <kallumtolkien@gmail.com>
2021-03-03 21:36:07 +0000

    Reached Feeding the Cat section

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const { Cat } = require('./models');
 
 const app = express();
 
 // we expect to have to parse json from request bodies
 app.use(express.json());
+app.post('/cats', (req, res) => {Cat.create(req.body).then(cat => res.status(201).json(cat))});
+app.get('/cats', (req, res) => {Cat.findAll(req.body).then(cat => res.status(201).json(cat))});
+app.get('/cats/:catId', (req, res) => {
+    Cat.findByPk(req.params.catId).then(cat => res.status(201).json(cat))});
 
 // we will put our routes and controller functions here
 
